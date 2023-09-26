5b68376aee6503b61cbc2b7cf94cc36119a460b3
mzffrtp <mzffr.tpgl@gmail.com>
2023-09-26 15:15:43 +0200

    header component

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -1,3 +1,24 @@
+import { Link } from "react-router-dom";
+import CustomButton from "../custom-button";
+
 export default function Header() {
-  return <div>header</div>;
+  return (
+    <header className="w-full absolute z-10">
+      <nav className="max-w-[1440px] mx-auto flex justify-between items-center sm:px-16 px-6 py-4">
+        <Link to={"/"} className="flex justify-center items-center">
+          <img
+            width={118}
+            height={18}
+            className="object-contain"
+            src="/logo.svg"
+          />
+        </Link>
+        <CustomButton
+          title="Sign In"
+          btnType="button"
+          designs="bg-primary-blue rounded-full text-white min-w-[130px] hover:bg-green-200 hover:text-slate-900"
+        />
+      </nav>
+    </header>
+  );
 }
